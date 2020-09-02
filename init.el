f362562f4ad1fdd33d9fdf06278fa088b3c876b8
wang1zhen <wang1zhendire@hotmail.com>
2020-09-02 22:40:58 +0800

    E24

--- init.el
+++ init.el
@@ -15,6 +15,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-default-note-file (concat org-directory "/notes.org"))
+ '(org-directory "~/Dropbox/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-starts t)
+ '(org-startup-folded 'overview)
+ '(org-startup-indented t)
  '(package-selected-packages
    '(iedit expand-region hungry-delete beacon undo-tree yasnippet jedi flycheck use-package try tabbar org-bullets counsel color-theme-modern auto-complete atom-one-dark-theme ace-window)))
 (custom-set-faces
